ce4f5649a664698173336518321c8d67119fdb76
Shaun <shaun@thenetninja.co.uk>
2022-06-06 17:45:30 +0100

    lesson-5

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-  res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+  const {title, load, reps} = req.body
+  
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 })
 
 // DELETE a workout

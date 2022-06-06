8d261975f1bf9e82bf5c0a34d3c096124af1d5c4
Shaun <shaun@thenetninja.co.uk>
2022-06-06 17:17:50 +0100

    lesson-2

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})

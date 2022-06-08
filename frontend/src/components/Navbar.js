525dc253040b46408562b06222e3fdd4ba7c1587
Shaun <shaun@thenetninja.co.uk>
2022-06-08 19:07:37 +0100

    lesson-8

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+
+  return (
+    <div className="home">
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Home

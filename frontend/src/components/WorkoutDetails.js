623acdba822991c94769017d32e0bfaec99b6e15
Shaun <shaun@thenetninja.co.uk>
2022-06-09 11:44:52 +0100

    lesson-12

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,18 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
 
   return (
     <div className="workout-details">
@@ -6,6 +20,7 @@ const WorkoutDetails = ({ workout }) => {
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   )
 }
--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -12,6 +12,10 @@ export const workoutsReducer = (state, action) => {
       return { 
         workouts: [action.payload, ...state.workouts] 
       }
+    case 'DELETE_WORKOUT':
+      return { 
+        workouts: state.workouts.filter(w => w._id !== action.payload._id) 
+      }
     default:
       return state
   }

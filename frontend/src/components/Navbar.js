0970db0c1909053e9c78eaffe5a8693d7198e179
Piyush0521 <piyush5544agarwal@gmail.com>
2022-12-19 15:30:01 +0530

    initial

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,64 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import {
+  BreadcrumbItem,
+  Breadcrumb,
+  BreadcrumbLink,
+  Flex,
+  Heading,
+  Spacer,
+  Divider,
+  Box,
+} from "@chakra-ui/react";
+import {
+  //   AddIcon,
+  //   ChevronDownIcon,
+  ChevronRightIcon,
+  //   HamburgerIcon,
+  //   PhoneIcon,
+} from "@chakra-ui/icons";
 const Navbar = () => {
-
   return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Workout Buddy</h1>
-        </Link>
-      </div>
-    </header>
-  )
-}
+    <Box>
+      <Flex align="center" mx={[5, 10]} mt={[10, 20]} px={[3, 8]}>
+        <Box>
+          <Heading fontSize={[22, 42]} fontFamily="Pacifico">
+            Blog SPARK
+          </Heading>
+        </Box>
+        <Spacer />
+        <Breadcrumb
+          fontWeight="bold"
+          fontFamily="Quicksand"
+          // color="#CCCFDC"
+          alignSelf="flex-end"
+          spacing={[0, 2]}
+          separator={<ChevronRightIcon color="black" />}
+        >
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/authors/" fontSize={[12, 20]}>
+              Authors
+            </BreadcrumbLink>
+          </BreadcrumbItem>
 
-export default Navbar
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/about" fontSize={[12, 20]}>
+              About
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/newBlog" fontSize={[12, 20]}>
+              {" "}
+              New Blog
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Flex>
+      <Box mx={[5, 10]} mt={[4, 14]}>
+        <Divider borderColor={"gray"} borderWidth="1px" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;

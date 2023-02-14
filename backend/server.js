e121ba526faf8e9788368de5c11f33e013d68ba4
codetheo75 <52568022+codetheo75@users.noreply.github.com>
2023-02-14 14:14:54 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
+mongoose.set('strictQuery', false);
+
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {

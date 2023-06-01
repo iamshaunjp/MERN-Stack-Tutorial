2ce95ab0743bb82f36d888755976ab2aa386760b
anonGTF <anongtf26@gmail.com>
2023-06-02 00:50:55 +0700

    set CORS

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require("cors");
 const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(cors({
+  origin: "*",
+}))
 app.use(express.json())
 
 app.use((req, res, next) => {

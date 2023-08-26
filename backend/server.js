6441b794ac54db01f65a2f1439cabe7579370fe7
editor-taka <takachan1050150@gmail.com>
2023-08-26 14:51:19 +0900

    fix require bug

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
-import express from "express"
+const express = require("express")
+const dotenv = require("dotenv")
 
+dotenv.config()
+// express app
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+  next()
+})
+
+
+app.get("/", (req, res)=>{
+  res.status(200).json({status: true, msg: "Hello World"})
+})
+
 // listen for request
-app.listen()
+app.listen(process.env.PORT, ()=> {
+  console.log("Listening on port", process.env.PORT)
+})
+
